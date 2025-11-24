2252a93874bde7ecd9b15235331df33648ad586d
F2RUZ <gaffarovferuz4@gmail.com>
2025-11-24 20:01:45 +0500

    salom

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="h-[150px] bg-gray-900"></div>;
+};
+
+export default Footer;
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="w-full h-[80vh] bg-amber-900">
+      <h1>About page</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
